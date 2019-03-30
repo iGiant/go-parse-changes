e3eb1bb696fb6801d05f49f6b7781ee0b6754253
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-03-30 19:26:44 +0400

    structure + python.ini

--- parse.go
+++ parse.go
@@ -11,17 +11,22 @@ import (
 	"mylibs/slkclient"
 )
 
+type iniParam struct {
+	file, url, selectors, attr, head, text string
+	start, end, number int
+}
+
 const
 	ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
 		 "Chrome/73.0.3683.46 Safari/537.36 OPR/60.0.3255.8 (Edition beta)"
 
-func parsing(url, selectors, attr string, num int) (string, error) {
+func parsing(ip iniParam) (string, error) {
 	var (
 		result []string
 		item string
 	)
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", ip.url, nil)
 	if err != nil {
 		return "", fmt.Errorf("Error reading request: %v", err)
 	}
@@ -44,12 +49,12 @@ func parsing(url, selectors, attr string, num int) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("Error reading body: %v", err)
 	}
-	doc.Find(selectors).Each(func(i int, s *goquery.Selection) {
+	doc.Find(ip.selectors).Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
-		if attr == "text" {
+		if ip.attr == "text" {
 			item = s.Text()
 		} else {
-			item, _ = s.Attr(attr)
+			item, _ = s.Attr(ip.attr)
 		}
 		result = append(result, item)
 
@@ -57,30 +62,40 @@ func parsing(url, selectors, attr string, num int) (string, error) {
 	if len(result) == 0 {
 		return "", fmt.Errorf("No items found")
 	}
-	if num < 0 {
-		num = len(result) + num
+	if ip.number < 0 {
+		ip.number = len(result) + ip.number
 	}
-	// fmt.Println(num, len(result))
-	return result[num], nil
+	if ip.start != -1 && ip.end != -1 && ip.end > ip.start {
+		return result[ip.number][ip.start:ip.end], nil
+	}
+	return result[ip.number], nil
 }
 
-func getIni(filename string) (file, url, selectors, attr string,
-							  number int, head, text string, err error) {
+func getIni(filename string) (ip iniParam, err error) {
 	cfg, err := ini.Load(filename)
     if err != nil {
         return
 	}
-	file = cfg.Section("main").Key("file").String()
-	url = cfg.Section("main").Key("url").String()
-	selectors = cfg.Section("main").Key("selectors").String()
-	selectors = strings.ReplaceAll(selectors, "№", "#")
-	attr = cfg.Section("main").Key("attr").String()
-	number, err = cfg.Section("main").Key("number").Int()
+
+	ip.file = cfg.Section("main").Key("file").String()
+	ip.url = cfg.Section("main").Key("url").String()
+	ip.selectors = cfg.Section("main").Key("selectors").String()
+	ip.selectors = strings.ReplaceAll(ip.selectors, "№", "#")
+	ip.attr = cfg.Section("main").Key("attr").String()
+	ip.start, err = cfg.Section("main").Key("start").Int()
 	if err != nil {
         return
 	}
-	head = cfg.Section("main").Key("head").String()
-	text = cfg.Section("main").Key("text").String()
+	ip.end, err = cfg.Section("main").Key("end").Int()
+	if err != nil {
+        return
+	}
+	ip.number, err = cfg.Section("main").Key("number").Int()
+	if err != nil {
+        return
+	}
+	ip.head = cfg.Section("main").Key("head").String()
+	ip.text = cfg.Section("main").Key("text").String()
 	return
 }
 
@@ -97,26 +112,27 @@ func main() {
 	filenames, err := ioutil.ReadDir(".")
 	if err == nil {
 		var (
-			file, url, selectors, attr, head, text, newtext, oldtext string
-			number int
+			ip iniParam
+			newtext, oldtext string
 			err error
 		)
 		for _, filename := range filenames {
 			if strings.HasSuffix(filename.Name(), ".ini") {
-				file, url, selectors, attr, number, head, text, err = getIni(filename.Name())
+				ip, err = getIni(filename.Name())
 				if err != nil {
 					continue
 				}
-				newtext, err = parsing(url, selectors, attr, number)
+				newtext, err = parsing(ip)
 				if err != nil {
 					continue
 				}
-				oldtext, _ = getOldText(file)
+				oldtext, _ = getOldText(ip.file)
 				if newtext != oldtext {
-					slkclient.SendToSlack(head, fmt.Sprintf(text, newtext), "", "", "")
-					ioutil.WriteFile(file, []byte(newtext), 0666)
+					slkclient.SendToSlack(ip.head, fmt.Sprintf(ip.text, newtext), "", "", "")
+					ioutil.WriteFile(ip.file, []byte(newtext), 0666)
 				}
 			}
 	}
 }
 }
+// go build -ldflags "-H windowsgui" <file.go>
